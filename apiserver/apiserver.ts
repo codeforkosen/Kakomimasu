614bf0f9e85197206b19db56103d47af05647179
Suzu Tomo <47011206+kamekyame@users.noreply.github.com>
2020-07-02 20:06:47 +0900

    Fix: apiserver

--- apiserver/apiserver.ts
+++ apiserver/apiserver.ts
@@ -3,26 +3,13 @@ import {
   createRouter,
   contentTypeFilter,
   createApp,
-} from "https://servestjs.org/@v1.0.0/mod.ts";
+} from "https://servestjs.org/@v1.1.0/mod.ts";
 
 import { Kakomimasu, Board, Action } from "../Kakomimasu.mjs";
 
 const kkmm = new Kakomimasu();
 
 // #region プレイヤー登録・ルームID取得API
-
-const createDefaultBoard = () => {
-  const w = 8;
-  const h = 8;
-  const points = [];
-  for (let i = 0; i < w * h; i++) {
-    points[i] = i;
-    // points[i] = i % (16 * 2 + 1) - 16;
-    // util.rnd(16 * 2 + 1) - 16;
-  }
-  const nagent = 6;
-  return new Board(w, h, points, nagent);
-};
 
 const addPlayer = (playerName: string, spec: string) => {
   const player = kkmm.createPlayer(playerName, spec);
@@ -37,26 +24,19 @@ const addPlayer = (playerName: string, spec: string) => {
 };
 
 export class PlayerPost {
-  constructor(public playerName: string, public spec: string) {
-    this.playerName = playerName;
-    this.spec = spec;
-  }
+  constructor(public name: string, public spec: string) {}
 }
 
 export const newPlayerPost = async (req: ServerRequest) => {
-  console.log(req.match);
-
   console.log(req, "newPlayer");
-  const bodyJson = (await req.json()) as PlayerPost;
-  const player = addPlayer(bodyJson.playerName, bodyJson.spec);
-  //players.push(player);
-
+  const playerPost = (await req.json()) as PlayerPost;
+  const player = addPlayer(playerPost.name, playerPost.spec);
   await req.respond({
     status: 200,
     headers: new Headers({
       "content-type": "application/json",
     }),
-    body: JSON.stringify(player, ["uuid", "name", "spec"]),
+    body: JSON.stringify(player.getJSON()),
   });
 };
 
@@ -81,12 +61,7 @@ export const getGameInfo = async (req: ServerRequest) => {
 export const routes = () => {
   const router = createRouter();
 
-  //router.get("match", contentTypeFilter("application/json"), newPlayerPost);
-  //router.post("match", newPlayerPost);
-  router.handle(
-    new RegExp("^match\?(.+)"),
-    newPlayerPost,
-  );
+  router.handle("match", contentTypeFilter("application/json"), newPlayerPost);
 
   router.get(new RegExp("^match/(.\\d+?)$"), getGameInfo);
 
@@ -97,3 +72,16 @@ export const routes = () => {
 const app = createApp();
 app.route("/", routes());
 app.listen({ port: 8880 });
+
+const createDefaultBoard = () => {
+  const w = 8;
+  const h = 8;
+  const points = [];
+  for (let i = 0; i < w * h; i++) {
+    points[i] = i;
+    // points[i] = i % (16 * 2 + 1) - 16;
+    // util.rnd(16 * 2 + 1) - 16;
+  }
+  const nagent = 6;
+  return new Board(w, h, points, nagent);
+};
