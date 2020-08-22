9469b2ddb62f41cbebbe2523f51d37cb40f1d6f3
Kizitora1777 <m18028@g.metro-cit.ac.jp>
2020-08-22 14:42:54 +0900

    add get関数およびpost関数の返り値をJSON型文字列からjsonオブジェクトに変更した。

--- client_c++/action.cpp
+++ client_c++/action.cpp
@@ -1,18 +1,17 @@
 #include "http_methods.hpp"
-#include "json.hpp"
 using namespace std;
 
 const string host = "http://localhost:8880";
 
 // 指定した名前のユーザーが存在するかどうか調べる
 // 存在しない場合は「{"error":"Can not find user."}」を返す
-string userShow(const string name) {
+nlohmann::json userShow(const string name) {
     return http_methods::get(host, "/users/show/" + name);
 }
 
 // 3つの引数を元にjsonデータを作成し、サーバーに登録する
 // 登録に成功した場合はjsonデータを、失敗した場合は「error」を返す
-string userRegist(const string sn, const string n, const string p) {
+nlohmann::json userRegist(const string sn, const string n, const string p) {
     nlohmann::json user_info = {
         {"screenName", sn},
         {"name", n},
@@ -26,10 +25,10 @@ string userRegist(const string sn, const string n, const string p) {
 int main() {
     const string name = "kizitora";
     const string password = name + "-pw";
-    const string user = userShow(name);
-
+    
+    nlohmann::json user_info = userShow(name);
     // 指定した名前のユーザーがサーバーにいない場合は、ユーザー情報を作って登録する
-    if(user == "{\"error\":\"Can not find user.\"}") {
-        userRegist("kizitora", name, password);
+    if(user_info.dump() == "{\"error\":\"Can not find user.\"}") {
+        user_info = userRegist("kizitora", name, password);
     }
 }
