f35c8a63ff2d5f384c96ab9cd7c23fe05c9217d0
Kizitora1777 <m18028@g.metro-cit.ac.jp>
2020-08-22 14:20:14 +0900

    fix jsonという変数名と、jsonというクラス名が衝突しないために、「using json = nlohmann::json」の記述を削除した

--- client_c++/action.cpp
+++ client_c++/action.cpp
@@ -1,7 +1,6 @@
 #include "http_methods.hpp"
 #include "json.hpp"
 using namespace std;
-using json = nlohmann::json;
 
 const string host = "http://localhost:8880";
 
@@ -14,12 +13,12 @@ string userShow(const string name) {
 // 3つの引数を元にjsonデータを作成し、サーバーに登録する
 // 登録に成功した場合はjsonデータを、失敗した場合は「error」を返す
 string userRegist(const string sn, const string n, const string p) {
-    json user_info = {
+    nlohmann::json user_info = {
         {"screenName", sn},
         {"name", n},
         {"password", p}
     };
-    
+
     // dumpメソッドを使うことで、jsonインスタンスをJSON形式の文字列に変換する
     return http_methods::post(host, "/users/regist", user_info.dump());
 }
@@ -28,7 +27,7 @@ int main() {
     const string name = "kizitora";
     const string password = name + "-pw";
     const string user = userShow(name);
-    
+
     // 指定した名前のユーザーがサーバーにいない場合は、ユーザー情報を作って登録する
     if(user == "{\"error\":\"Can not find user.\"}") {
         userRegist("kizitora", name, password);
