343232316fcb5bc85a107e22bff20ddce0fc2552
Kizitora1777 <m18028@g.metro-cit.ac.jp>
2020-08-22 14:17:47 +0900

    add userRegist関数がjonデータを返すようにした

--- client_c++/action.cpp
+++ client_c++/action.cpp
@@ -12,15 +12,16 @@ string userShow(const string name) {
 }
 
 // 3つの引数を元にjsonデータを作成し、サーバーに登録する
-void userRegist(const string sn, const string n, const string p) {
+// 登録に成功した場合はjsonデータを、失敗した場合は「error」を返す
+string userRegist(const string sn, const string n, const string p) {
     json user_info = {
         {"screenName", sn},
         {"name", n},
         {"password", p}
     };
-
+    
     // dumpメソッドを使うことで、jsonインスタンスをJSON形式の文字列に変換する
-    string res = http_methods::post(host, "/users/regist", user_info.dump());
+    return http_methods::post(host, "/users/regist", user_info.dump());
 }
 
 int main() {
