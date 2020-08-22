864602ac30aee2bef24cd3c5afb87c523083a794
Kizitora1777 <m18028@g.metro-cit.ac.jp>
2020-08-22 15:14:45 +0900

    post/get関数およびそれらに付随していた関数をclient_util.hppにまとめた

--- client_c++/action.cpp
+++ client_c++/action.cpp
@@ -1,34 +1,17 @@
-#include "http_methods.hpp"
+#include "client_util.hpp"
 using namespace std;
 
-const string host = "http://localhost:8880";
-
-// 指定した名前のユーザーが存在するかどうか調べる
-// 存在しない場合は「{"error":"Can not find user."}」を返す
-nlohmann::json userShow(const string name) {
-    return http_methods::get(host, "/users/show/" + name);
-}
-
-// 3つの引数を元にjsonデータを作成し、サーバーに登録する
-// 登録に成功した場合はjsonデータを、失敗した場合は「error」を返す
-nlohmann::json userRegist(const string sn, const string n, const string p) {
-    nlohmann::json user_info = {
-        {"screenName", sn},
-        {"name", n},
-        {"password", p}
-    };
-
-    // dumpメソッドを使うことで、jsonインスタンスをJSON形式の文字列に変換する
-    return http_methods::post(host, "/users/regist", user_info.dump());
-}
-
 int main() {
+    const string screenName = "kizitora";
     const string name = "kizitora";
     const string password = name + "-pw";
+    const string spec = "AI";
     
     nlohmann::json user_info = userShow(name);
     // 指定した名前のユーザーがサーバーにいない場合は、ユーザー情報を作って登録する
     if(user_info.dump() == "{\"error\":\"Can not find user.\"}") {
-        user_info = userRegist("kizitora", name, password);
+        user_info = userRegist(screenName, name, password);
     }
+    
+    //nlohmann::json match_info = match(user_info["id"], password, spec);
 }
