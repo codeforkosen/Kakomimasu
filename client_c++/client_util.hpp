24dc4bc78b4873638b5e39eaa79df731afd93645
Kizitora1777 <m18028@g.metro-cit.ac.jp>
2020-08-22 15:14:45 +0900

    post/get関数およびそれらに付随していた関数をclient_util.hppにまとめた

--- client_c++/client_util.hpp
+++ client_c++/client_util.hpp
@@ -6,6 +6,8 @@
 #define MAX_LEN_JSON (200 * 1024) // 100kbyte
 #define LEN_TOKEN 6 // TOKENの長さは固定
 
+const std::string host = "http://localhost:8880";
+
 namespace http_methods {
 nlohmann::json post(const std::string host, const std::string path, const std::string json) {
     // 引数を元にcurlコマンドを文字列を作成
@@ -63,4 +65,23 @@ nlohmann::json get(const std::string host, const std::string path) {
   
     return response_json;
 }
+}
+
+// 指定した名前のユーザーが存在するかどうか調べる
+// 存在しない場合は「{"error":"Can not find user."}」を返す
+nlohmann::json userShow(const std::string name) {
+    return http_methods::get(host, "/users/show/" + name);
+}
+
+// 3つの引数を元にjsonデータを作成し、サーバーに登録する
+// 登録に成功した場合はjsonデータを、失敗した場合は「error」を返す
+nlohmann::json userRegist(const std::string sn, const std::string n, const std::string p) {
+    nlohmann::json user_info = {
+        {"screenName", sn},
+        {"name", n},
+        {"password", p}
+    };
+
+    // dumpメソッドを使うことで、jsonインスタンスをJSON形式の文字列に変換する
+    return http_methods::post(host, "/users/regist", user_info.dump());
 }
