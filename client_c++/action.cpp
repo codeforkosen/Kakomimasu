7d64a2f0724214219831e710d3de4edefff4a41f
Kizitora1777 <m18028@g.metro-cit.ac.jp>
2020-08-24 15:19:30 +0900

    match_info_format_to_solver_input_format関数を実装した

--- client_c++/action.cpp
+++ client_c++/action.cpp
@@ -6,9 +6,9 @@ class data_to_input_in_solver {
 public:
     int height, width, each_agent_num, max_turn, max_time_limit_ms, now_turn;
     vector<vector<int>> points, tile;
-    vector<pair<int, int>> agent_positions;
+    vector<vector<pair<int, int>>> agent_positions;
 
-    data_to_input_in_solver(int h, int w, int a, int max_t, int s, vector<vector<int>> p, int now_t, vector<vector<int>> f, vector<pair<int, int>> ap) {
+    data_to_input_in_solver(int h, int w, int a, int max_t, int s, vector<vector<int>> p, int now_t, vector<vector<int>> f, vector<vector<pair<int, int>>> ap) {
         height = h;
         width = w;
         each_agent_num = a;
@@ -19,6 +19,54 @@ public:
         agent_positions = ap;
     }
 };
+
+data_to_input_in_solver match_info_format_to_solver_input_format(const nlohmann::json match_info) {
+    int h = match_info["board"]["height"];
+    int w = match_info["board"]["width"];
+    int a = match_info["board"]["nAgent"];
+    int s = (int)match_info["nextTurnUnixTime"] - (int)match_info["startedAtUnixTime"];
+    int max_t = match_info["totalTurn"];
+    
+    // 盤面の得点情報が h * w の長さの一次元配列で並べられているので、縦h、横wの二次元配列に収まるように変換する
+    vector<vector<int>> p(h, vector<int>(w));
+    int i = 0, j = 0;
+    for (int k = 0; k < h * w; k++) {
+        p[i][j] = match_info["board"]["points"][k];
+
+        if(j == (w - 1)) i++, j = 0;
+        else j++;
+    }
+
+    int now_t = match_info["turn"];
+
+    // タイルの状態を一次元配列から二次元配列に変換し、タイルの状態を表す値もソルバーのフォーマットに変更する
+    i = 0, j = 0;
+    vector<vector<int>> f(h, vector<int>(w));
+    for (int k = 0; k < h * w; k++) {
+        int l = match_info["tiled"][k][0];
+        int r = match_info["tiled"][k][1];
+
+        if(r == -1) f[i][j] = 0;
+        else if (l == 0 &&  r == 0) f[i][j] = 1;
+        else if (l == 1 &&  r == 0) f[i][j] = 2;
+        else if (l == 0 &&  r == 1) f[i][j] = 3;
+        else if (l == 1 &&  r == 1) f[i][j] = 4;
+
+        if(j == (w - 1)) i++, j = 0;
+        else j++;
+    }
+
+    vector<vector<pair<int, int>>> ap(2, vector<pair<int, int>>(a));
+    for (int team = 0; team <= 1; team++) {
+        for (int i = 0; i < a; ++i) {
+            //cout << match_info["players"][team]["agents"][i]["x"] << " " << match_info["players"][team]["agents"][i]["y"] << endl;
+            ap[team][i] = make_pair(match_info["players"][team]["agents"][i]["x"], match_info["players"][team]["agents"][i]["y"]);
+        }
+    }
+
+    data_to_input_in_solver res(h, w, a, s, max_t, p, now_t, f, ap);
+    return res;
+}
 
 int main() {
     const string screenName = "kizitora";
@@ -41,6 +89,6 @@ int main() {
         game_info = getGameInfo(gameId);
         sleep_ms(100);
     } while (game_info["gaming"] == false);
-
     
+    data_to_input_in_solver input_datas = match_info_format_to_solver_input_format(getGameInfo(gameId));
 }
