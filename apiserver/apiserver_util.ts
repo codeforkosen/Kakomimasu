2c5b34035df32684084fdcf133558a3ed9fa5a7a
Suzu Tomo <47011206+kamekyame@users.noreply.github.com>
2021-02-13 02:31:51 +0900

    Fix pathResolverがlinux上で正しく処理できないバグを修正

--- apiserver/apiserver_util.ts
+++ apiserver/apiserver_util.ts
@@ -1,3 +1,5 @@
+import { fromFileUrl } from "https://deno.land/std@0.79.0/path/mod.ts";
+
 export const jsonResponse = (json: any) => {
   return {
     status: 200,
@@ -31,15 +33,5 @@ export const readJsonFileSync = (path: string | URL) => {
 };
 
 export function pathResolver(meta: ImportMeta): (p: string) => string {
-  return (p) => {
-    let cwd = meta.url.substring(8, meta.url.lastIndexOf("/")); // Deno.cwd();
-    if (p.startsWith("./")) {
-      return cwd + p.substr(1, p.length);
-    } else if (p.startsWith("../")) {
-      cwd = cwd.substring(0, cwd.lastIndexOf("/"));
-      return cwd + p.substr(2, p.length);
-    } else {
-      return cwd + p;
-    }
-  };
+  return (p) => fromFileUrl(new URL(p, meta.url));
 }
