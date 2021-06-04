0c4ab74a7937ffe9e993984b3bd3305455f9c5b9
Suzu Tomo <47011206+kamekyame@users.noreply.github.com>
2021-06-04 17:31:14 +0900

    テストが通らない問題を解消

---
+++ apiserver/parts/wait-start.ts
@@ -0,0 +1,12 @@
+const sleep = (msec: number) =>
+  new Promise((resolve) => setTimeout(resolve, msec));
+
+while (true) {
+  try {
+    await Deno.connectTls({ port: 8880, hostname: "localhost" });
+    break;
+  } catch (e) {
+    console.log(".");
+    await sleep(1000);
+  }
+}
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,8 @@
 #!/bin/sh -l
 
+deno -V
 deno cache ./apiserver/apiserver.ts
+deno cache  --no-check --import-map ./pages/import-map.json ./pages/route.tsx
 deno run -A ./apiserver/apiserver.ts &
+deno run -A ./apiserver/parts/wait-start.ts
 deno test -A
