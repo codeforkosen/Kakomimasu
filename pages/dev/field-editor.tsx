b3356e359f4ef0b54d7d3bd024fd97134f188971
Suzu Tomo <47011206+kamekyame@users.noreply.github.com>
2021-06-04 15:56:15 +0900

    fieldEditorが動作しないバグを修正

--- pages/dev/field-editor.tsx
+++ pages/dev/field-editor.tsx
@@ -1,6 +1,6 @@
 /// <reference lib="dom"/>
 import React, { useEffect, useState } from "react";
-import { createStyles, makeStyles } from "@material-ui/core/styles";
+import { createStyles, makeStyles } from "@material-ui/styles";
 import { Link, Redirect, RouteComponentProps } from "react-router-dom";
 import TextField from "@material-ui/core/TextField";
 import MenuItem from "@material-ui/core/MenuItem";
@@ -11,7 +11,9 @@ import GameBoard from "../../components/gameBoard.tsx";
 import ApiClient from "../../apiserver/api_client.js";
 const apiClient = new ApiClient("");
 
-const useStyles = makeStyles((theme) =>
+import { Board } from "../../apiserver/types.ts";
+
+const useStyles = makeStyles(
   createStyles({
     content: {
       display: "flex",
@@ -21,24 +23,22 @@ const useStyles = makeStyles((theme) =>
     pieGraph: {
       height: 300,
     },
-  })
+  }),
 );
 
 export default function () {
   const classes = useStyles();
-  const [boards, setBoards] = useState<any[]>();
-  const [board, setBoard] = useState<any[]>();
+  const [boards, setBoards] = useState<Board[]>();
   const [game, setGame] = useState<any>();
-  const gameF = React.useRef(game);
 
   useEffect(() => {
     getBoards();
   }, []);
 
   async function getBoards() {
-    const boards_ = await apiClient.getBoards();
-    console.log(boards_);
-    setBoards(boards_);
+    const res = await apiClient.getBoards();
+    console.log();
+    if (res.success) setBoards(res.data);
   }
 
   const handleChange = (
@@ -47,7 +47,7 @@ export default function () {
     const value = event.target.value;
     if (!boards) return;
     const board = boards.find((b) => b.name === value);
-    setBoard(board);
+    if (!board) return;
     const tiled = new Array(board.height * board.width);
     for (let i = 0; i < tiled.length; i++) {
       tiled[i] = [0, -1];
@@ -58,47 +58,6 @@ export default function () {
       players: [{ agents: [] }, { agents: [] }],
     };
     setGame(game);
-  };
-  const mouseDown = (e: MouseEvent, i: number, x: number, y: number) => {
-    const tiled = [...gameF.current.tiled];
-    const players = [...gameF.current.players];
-
-    const isAgent = (x: number, y: number) => {
-      if (gameF.current.players) {
-        const agent = (gameF.current.players as any[]).map((e: any, i) =>
-          (e.agents as any[]).map((e_, j) => {
-            return { agent: e_, player: i, n: j };
-          })
-        ).flat().find((e) => e.agent.x === x && e.agent.y === y);
-        return agent;
-      } else return undefined;
-    };
-
-    switch (e.button) {
-      case 0: {
-        let t = tiled[i];
-        let a = (t[0] * 2 + t[1]) + 1;
-        a = (a + 1) % 5;
-        t = [Math.trunc((a - 1) / 2), (a - 1) % 2];
-        tiled[i] = t;
-
-        const agent = isAgent(x, y);
-        if (agent) {
-          players[agent.player].agents.splice(agent.n, 1);
-        }
-        break;
-      }
-      case 2: {
-        let t = tiled[i];
-        if (t[0] === 1) {
-          const a = isAgent(x, y);
-          if (a) players[a.player].agents.splice(a.n, 1);
-          else players[t[1]].agents.push({ x, y });
-        }
-        break;
-      }
-    }
-    setGame({ ...gameF.current, tiled, players });
   };
 
   useEffect(() => {
@@ -112,10 +71,48 @@ export default function () {
       const td = tds[i];
       td.oncontextmenu = () => false;
       td.onmousedown = (e) => {
-        mouseDown(e, i, x, y);
+        const tiled = [...game.tiled];
+        const players = [...game.players];
+
+        const isAgent = (x: number, y: number) => {
+          if (game.players) {
+            const agent = (game.players as any[]).map((e: any, i) =>
+              (e.agents as any[]).map((e_, j) => {
+                return { agent: e_, player: i, n: j };
+              })
+            ).flat().find((e) => e.agent.x === x && e.agent.y === y);
+            return agent;
+          } else return undefined;
+        };
+
+        switch (e.button) {
+          case 0: {
+            let t = tiled[i];
+            let a = (t[0] * 2 + t[1]) + 1;
+            a = (a + 1) % 5;
+            t = [Math.trunc((a - 1) / 2), (a - 1) % 2];
+            tiled[i] = t;
+
+            const agent = isAgent(x, y);
+            if (agent) {
+              players[agent.player].agents.splice(agent.n, 1);
+            }
+            break;
+          }
+          case 2: {
+            let t = tiled[i];
+            if (t[0] === 1) {
+              const a = isAgent(x, y);
+              if (a) players[a.player].agents.splice(a.n, 1);
+              else players[t[1]].agents.push({ x, y });
+            }
+            break;
+          }
+        }
+        setGame({ ...game, tiled, players });
       };
     }
-  }, [board]);
+  });
 
   return (
     <Content title="フィールド説明用エディタ">
