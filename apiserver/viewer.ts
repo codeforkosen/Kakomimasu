cb49b09a4e6e0ca798ce74543b5cbe48521e2b0d
Suzu Tomo <47011206+kamekyame@users.noreply.github.com>
2021-06-06 21:06:46 +0900

    test時にビューアを起動しないように変更

--- apiserver/viewer.ts
+++ apiserver/viewer.ts
@@ -1,3 +1,5 @@
+import { parse } from "https://deno.land/std@0.84.0/flags/mod.ts";
+const args = parse(Deno.args);
 import {
   createRouter,
   serveStatic,
@@ -9,13 +11,17 @@ const resolve = util.pathResolver(import.meta);
 import * as esbuild from "https://deno.land/x/esbuild@v0.11.17/mod.js";
 import { denoPlugin } from "https://raw.githubusercontent.com/lucacasonato/esbuild_deno_loader/fa2219c3df9494da6c33e3e4dffb1a33b5cc0345/mod.ts";
 
-const bundle = await esbuild.build({
-  entryPoints: ["file:///" + resolve("../pages/route.tsx")],
-  plugins: [denoPlugin({ importMapFile: resolve("../pages/import-map.json") })],
-  bundle: true,
-  outfile: resolve("../public/app.bundle.js"),
-  minify: true,
-});
+if (!args.noViewer) {
+  const bundle = await esbuild.build({
+    entryPoints: ["file:///" + resolve("../pages/route.tsx")],
+    plugins: [
+      denoPlugin({ importMapFile: resolve("../pages/import-map.json") }),
+    ],
+    bundle: true,
+    outfile: resolve("../public/app.bundle.js"),
+    minify: true,
+  });
+}
 
 export const viewerRoutes = () => {
   const router = createRouter();
--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,6 @@
 
 deno -V
 deno cache ./apiserver/apiserver.ts
-deno cache  --no-check --import-map ./pages/import-map.json ./pages/route.tsx
-deno run -A ./apiserver/apiserver.ts &
+deno run -A ./apiserver/apiserver.ts --noViewer &
 deno run -A ./apiserver/parts/wait-start.ts
 deno test -A
