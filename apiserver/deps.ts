803637cc600898934ba584d11f788ae3eaac29aa
Suzu Tomo <47011206+kamekyame@users.noreply.github.com>
2021-06-19 19:10:54 +0900

    fix lint warning jwt.ts

--- apiserver/deps.ts
+++ apiserver/deps.ts
@@ -10,7 +10,7 @@ export { v4 } from "https://deno.land/std@0.97.0/uuid/mod.ts";
 // Third Party Modules
 export * from "https://deno.land/x/dotenv@v2.0.0/mod.ts";
 export * from "https://deno.land/x/servest@v1.3.1/mod.ts";
-export { decode } from "https://deno.land/x/djwt@v2.2/mod.ts";
+export * from "https://deno.land/x/djwt@v2.2/mod.ts";
 export * as esbuild from "https://deno.land/x/esbuild@v0.11.17/mod.js";
 
 export { denoPlugin } from "https://raw.githubusercontent.com/lucacasonato/esbuild_deno_loader/fa2219c3df9494da6c33e3e4dffb1a33b5cc0345/mod.ts";
