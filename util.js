90a5a845d3c1330565a78937ca2d24f46d858b7c
Suzu Tomo <47011206+kamekyame@users.noreply.github.com>
2021-06-20 14:48:48 +0900

    fix lint warning util.js

--- util.js
+++ util.js
@@ -19,14 +19,14 @@ util.deepEquals = (x, y) => {
   if (!(x instanceof Object) || !(y instanceof Object)) return false;
   if (x.constructor !== y.constructor) return false;
   for (const p in x) {
-    if (!x.hasOwnProperty(p)) continue;
-    if (!y.hasOwnProperty(p)) return false;
+    if (!Object.prototype.hasOwnProperty.call(x, p)) continue;
+    if (!Object.prototype.hasOwnProperty.call(y, p)) return false;
     if (x[p] === y[p]) continue;
     if (typeof (x[p]) !== "object") return false;
     if (!util.deepEquals(x[p], y[p])) return false;
   }
   for (const p in y) {
-    if (y.hasOwnProperty(p) && !x.hasOwnProperty(p)) return false;
+    if (Object.prototype.hasOwnProperty.call(y, p) && !Object.prototype.hasOwnProperty.call(x, p)) return false;
   }
   return true;
 };
