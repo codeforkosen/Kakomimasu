a8ddf58ad53a6e8866d000a011549bc090ab78fd
Suzu Tomo <47011206+kamekyame@users.noreply.github.com>
2021-07-29 21:05:58 +0900

    update: esbuils_deno_loader 0.1.1 -> 0.2.0

--- apiserver/deps.ts
+++ apiserver/deps.ts
@@ -12,4 +12,4 @@ export * from "https://deno.land/x/dotenv@v2.0.0/mod.ts";
 export * from "https://deno.land/x/servest@v1.3.2/mod.ts";
 export * from "https://deno.land/x/djwt@v2.2/mod.ts";
 export * as esbuild from "https://deno.land/x/esbuild@v0.12.15/mod.js";
-export { denoPlugin } from "https://deno.land/x/esbuild_deno_loader@0.1.1/mod.ts";
+export { denoPlugin } from "https://deno.land/x/esbuild_deno_loader@0.2.0/mod.ts";
