665c4a4ec3d834cf8e65ff9bec78938559ec1ec6
Suzu Tomo <47011206+kamekyame@users.noreply.github.com>
2021-08-19 18:39:01 +0900

    update x/djwt version from v2.2 to v2.3

--- apiserver/deps.ts
+++ apiserver/deps.ts
@@ -10,6 +10,6 @@ export * from "https://deno.land/std@0.101.0/ws/mod.ts";
 // Third Party Modules
 export * from "https://deno.land/x/dotenv@v2.0.0/mod.ts";
 export * from "https://deno.land/x/servest@v1.3.2/mod.ts";
-export * from "https://deno.land/x/djwt@v2.2/mod.ts";
+export * from "https://deno.land/x/djwt@v2.3/mod.ts";
 export * as esbuild from "https://deno.land/x/esbuild@v0.12.19/mod.js";
 export { denoPlugin } from "https://deno.land/x/esbuild_deno_loader@0.2.0/mod.ts";
