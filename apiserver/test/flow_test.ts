701c3016ae95b5aa6dcf17a9985ed82752a29572
Suzu Tomo <47011206+kamekyame@users.noreply.github.com>
2021-08-19 20:24:31 +0900

    fix flow_test bug

--- apiserver/test/flow_test.ts
+++ apiserver/test/flow_test.ts
@@ -171,7 +171,7 @@ Deno.test("send action(Turn 2)", async () => {
   }
   const gameInfo = res.data;
   if (!gameInfo.startedAtUnixTime) throw Error("startedAtUnixTime is null.");
-  await sleep(diffTime(gameInfo.startedAtUnixTime) + 1);
+  await sleep(diffTime(gameInfo.startedAtUnixTime) + 0.3);
   await ac.setAction(gameId, {
     actions: [{ agentId: 0, type: "PUT", x: 1, y: 2 }],
     index: 1,
@@ -179,7 +179,7 @@ Deno.test("send action(Turn 2)", async () => {
   //console.log(reqJson);
 
   if (!gameInfo.nextTurnUnixTime) throw Error("nextTurnUnixTime is null.");
-  await sleep(diffTime(gameInfo.nextTurnUnixTime) + 1);
+  await sleep(diffTime(gameInfo.nextTurnUnixTime) + 0.3);
   res = await ac.getMatch(gameId);
   if (res.success === false) {
     throw Error("Response Error. ErrorCode:" + res.data.errorCode);
