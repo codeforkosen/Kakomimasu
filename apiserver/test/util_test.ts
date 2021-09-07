875c449761f087a74760d132d76ad95e071c1201
Suzu Tomo <47011206+kamekyame@users.noreply.github.com>
2021-09-07 22:06:43 +0900

    Delete server related file

--- apiserver/test/util_test.ts
+++
@@ -1,7 +0,0 @@
-import { assert, v4 } from "../deps.ts";
-import { randomUUID } from "../apiserver_util.ts";
-
-Deno.test("randamUUID", () => {
-  const uuid = randomUUID();
-  assert(v4.validate(uuid));
-});
